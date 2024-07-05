5fe67140dfff73bcc465cbc203346e93e39e50c9
Richy Sel <alvincypher@gmail.com>
2024-07-05 11:11:05 +0000

    implemented clerk and convex auth

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// 
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://one-dingo-67.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
